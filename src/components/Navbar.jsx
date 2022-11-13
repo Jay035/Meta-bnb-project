a72c06d2236f4f13b0ebeaba4e211750fd035414
Jay035 <jaydencolins08@gmail.com>
2022-11-14 00:03:37 +0100

    feat: added modal on 'connect wallet' button click style: updated styles on the home screen and modal, made the pages and modal both responsive across devices

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,9 +1,10 @@
 import { useState } from "react";
 import { NavLink } from "react-router-dom";
 import logo from "../assets/logo.svg";
+import Modal from "./Modal";
 
 export default function Navbar() {
-  const [menuShown, setMenuShown] = useState(false);
+  const [menuState, setMenuState] = useState(false);
   const [modalState, setModalState] = useState(false);
 
   return (
@@ -13,7 +14,7 @@ export default function Navbar() {
       <nav
         className={`
           ${
-            menuShown
+            menuState
               ? `absolute top-0 left-0 bg-black/75 w-full flex flex-col gap-12 lg:pl-0 text-xl lg:items-center lg:flex-row`
               : `hidden lg:flex `
           } lg:relative`}
@@ -22,62 +23,67 @@ export default function Navbar() {
         <div className="flex flex-col gap-8 pt-8 pl-6 bg-white h-screen w-3/4 lg:pt-0 lg:pl-0 lg:w-full lg:h-fit lg:flex-row lg:items-center">
           <i
             className="ri-close-fill cursor-pointer text-5xl w-fit h-fit lg:hidden"
-            onClick={() => setMenuShown((prevState) => !prevState)}
+            onClick={() => setMenuState((prevState) => !prevState)}
           ></i>
           <ul className="flex flex-col gap-6 md:text-xl lg:flex-row lg:gap-12">
             <NavLink
               to="/"
               className="w-fit cursor-pointer hover:font-medium transition-colors"
+              onClick={() => setMenuState((prevState) => !prevState)}
             >
               Home
             </NavLink>
             <NavLink
               to="/places"
               className="w-fit cursor-pointer hover:font-medium transition-colors"
+              onClick={() => setMenuState((prevState) => !prevState)}
             >
               Place to stay
             </NavLink>
             <NavLink
               to="/NFTs"
               className="w-fit cursor-pointer hover:font-medium transition-colors"
+              onClick={() => setMenuState((prevState) => !prevState)}
             >
               NFTs
             </NavLink>
             <NavLink
               to="/community"
               className="w-fit cursor-pointer hover:font-medium transition-colors"
+              onClick={() => setMenuState((prevState) => !prevState)}
             >
               Community
             </NavLink>
           </ul>
-          <a
-            href=""
+          <button
             className="px-7 py-[13px] text-white rounded-xl bg-gradient-to-t from-[#a02279] to-[#a02279] w-fit hover:opacity-90 lg:hidden"
-            onClick={() => setModalState(prevState => !prevState)}
+            onClick={() => {
+              setMenuState(prevState => !prevState)
+              setModalState(prevState => !prevState);
+            }}
           >
             Connect wallet
-          </a>
+          </button>
         </div>
       </nav>
 
-      <a
-        href=""
+      <button
         className="px-7 py-[13px] text-center hidden lg:inline-block text-white rounded-xl bg-gradient-to-t from-[#a02279] to-[#a02279] w-fit hover:opacity-90"
         onClick={() => setModalState(prevState => !prevState)}
       >
         Connect wallet
-      </a>
+      </button>
 
       {/* hamburger icon */}
       <div
         className="cursor-pointer lg:hidden"
-        onClick={() => setMenuShown((prevState) => !prevState)}
+        onClick={() => setMenuState((prevState) => !prevState)}
       >
         <span className="w-6 h-[3px] block bg-black"></span>
         <span className="w-6 h-[3px] my-[3px] block bg-black"></span>
         <span className="w-6 h-[3px] block bg-black"></span>
       </div>
-      { modalState ? <Modal /> : null }
+      { modalState ? <Modal setModalState={setModalState} /> : null }
     </header>
   );
 }
